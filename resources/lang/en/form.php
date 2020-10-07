8ca618e518b6ea9e5c6d4dd11aac18614362f1b2
Nguyễn Viết Hiếu <36782331+hieunvph04479@users.noreply.github.com>
2020-10-07 11:22:02 +0700

    Cập nhật Translate cho core

---
+++ resources/lang/en/form.php
@@ -0,0 +1,70 @@
+<?php 
+
+return [
+	'create' 				=> 'Create',
+	'edit' 					=> 'Edit',
+
+	'require_text' 			=> 'Field * must required',
+
+	'metaseo' => [
+		'title_card' 		=> 'Seo',
+
+		'meta_title' 		=> 'Meta Title Tag (SEO)',
+		'meta_title_placeholder' => 'Title',
+		'meta_title_desc' 	=> 'characters. Title (meta_title) best about 60 - 70 characters',
+
+		'meta_description' 	=> 'Meta Description Tag (SEO)',
+		'meta_description_placeholder' => 'Description',
+		'meta_description_desc' => 'characters. Title (meta_description) best about 120 - 160 characters',
+
+		'meta_robots' 		=> 'Meta Robots Tag',
+		'meta_robots_desc' 	=> 'Mô tả trang',
+	],
+
+	'action' => [
+		'create' 			=> 'Create',
+		'create_and_exit' 	=> 'Create và Exit',
+		'save' 				=> 'Update',
+		'save_and_exit' 	=> 'Update và Exit',
+		'save_setting' 		=> 'Save',
+		'show' 				=> 'View',
+		'exit' 				=> 'Exit',
+	],
+
+	'valid' => [
+		'no_empty' 			=> 'is required!',
+		'is_select' 		=> 'is selected!',
+		'is_unique' 		=> 'is exists!',
+		'is_comfirm' 		=> 'is not match!',
+		'is_tags'			=> 'must have at least 1 value!',
+	],
+
+	'password_auto' 		=> 'Auto generate',
+	'password_generate' 	=> 'Password has been create, copy or click OK!',
+	'password_strength' 	=> 'Password must reach NORMAL level or higher',
+	'strength' 				=> 'Strength',
+	'too_weak' 				=> 'Too weak',
+	'weak' 					=> 'Weak',
+	'normal' 				=> 'Normal',
+	'strong' 				=> 'Strong',
+	'too_strong' 			=> 'Very Strong',
+
+	'add_tags' 				=> 'ADD',
+	'choose' 				=> 'Chose',
+	'no_data_finding' 		=> 'No Data Finding',
+	'search' 				=> 'Search',
+
+	'menu' => [
+		'structure' 		=> 'Menu structure',
+		'custom_link' 		=> 'Custom Link',
+		'fix_link' 			=> 'Fix Link',
+
+		'display_name' 		=> 'Display name',
+		'link' 				=> 'Link',
+		'open_direct' 		=> 'Open direct',
+		'open_blank' 		=> 'Open new Tab',
+		'no_rel' 			=> 'No',
+		'add_menu' 			=> 'Add Menu',
+		'close' 			=> 'Close',
+	],
+];
---
+++ resources/lang/vi/form.php
@@ -0,0 +1,70 @@
+<?php 
+
+return [
+	'create' 				=> 'Thêm mới',
+	'edit' 					=> 'Sửa',
+
+	'require_text' 			=> 'Trường có dấu * là bắt buộc',
+
+	'metaseo' => [
+		'title_card' 		=> 'Nội dung cho Seo',
+
+		'meta_title' 		=> 'Tiêu đề cho thẻ meta title (SEO)',
+		'meta_title_placeholder' => 'Tiêu đề trang',
+		'meta_title_desc' 	=> 'Ký tự. Tiêu đề (meta_title) tốt nhất khoảng 60 - 70 ký tự',
+
+		'meta_description' 	=> 'Nội dung cho thẻ meta description (SEO)',
+		'meta_description_placeholder' => 'Mô tả trang',
+		'meta_description_desc' => 'Ký tự. Mô tả (meta_description) tốt nhất khoảng 120 - 160 ký tự',
+
+		'meta_robots' 		=> 'Điều hướng Robots',
+		'meta_robots_desc' 	=> 'Mô tả trang',
+	],
+
+	'action' => [
+		'create' 			=> 'Thêm mới',
+		'create_and_exit'	=> 'Thêm và thoát',
+		'save' 				=> 'Cập nhật',
+		'save_and_exit' 	=> 'Cập nhật và thoát',
+		'save_setting' 		=> 'Lưu cấu hình',
+		'show' 				=> 'Xem',
+		'exit' 				=> 'Thoát',
+	],
+
+	'valid' => [
+		'no_empty' 			=> 'không được để trống!',
+		'is_select' 		=> 'phải được chọn!',
+		'is_unique' 		=> 'đã tồn tại!',
+		'is_comfirm' 		=> 'không trùng khớp!',
+		'is_tags' 			=> 'phải có ít nhất 1 giá trị!',
+	],
+
+	'password_auto' 		=> 'Tạo tự động',
+	'password_generate' 	=> 'Mật khẩu đã được tạo tự động, hãy sao chép và lưu lại!',
+	'password_strength' 	=> 'Mật khẩu phải đạt mức bình thường trở lên',
+	'strength' 				=> 'Độ mạnh',
+	'too_weak' 				=> 'Rất yếu',
+	'weak' 					=> 'Yếu',
+	'normal' 				=> 'Bình thường',
+	'strong' 				=> 'Mạnh',
+	'too_strong' 			=> 'Rất mạnh',
+
+	'add_tags' 				=> 'Thêm',
+	'choose' 				=> 'Chọn',
+	'no_data_finding' 		=> 'Không tìm thấy dữ liệu',
+	'search' 				=> 'Tìm kiếm',
+
+	'menu' => [
+		'structure' 		=> 'Cấu trúc menu',
+		'custom_link' 		=> 'Liên kết tự tạo',
+		'fix_link' 			=> 'Liên kết Cố định',
+
+		'display_name' 		=> 'Tên hiển thị',
+		'link' 				=> 'Đường dẫn',
+		'open_direct' 		=> 'Mở trực tiếp',
+		'open_blank' 		=> 'Mở tab mới',
+		'no_rel' 			=> 'Không',
+		'add_menu' 			=> 'Thêm menu',
+		'close' 			=> 'Đóng',
+	],
+];
